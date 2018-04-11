8c5b229be949e25e777938b721c45630cf3733f8
natalia <korgutlova2011@mail.ru>
2018-04-11 19:12:03 +0300

    Change database v.1.0

--- src/main/java/ru/kpfu/itis/group11501/smartmuseum/controller/SignInController.java
+++ src/main/java/ru/kpfu/itis/group11501/smartmuseum/controller/SignInController.java
@@ -1,16 +1,15 @@
 package ru.kpfu.itis.group11501.smartmuseum.controller;
 
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-        import org.springframework.security.crypto.password.PasswordEncoder;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestMethod;
-        import ru.kpfu.itis.group11501.smartmuseum.model.User;
-        import ru.kpfu.itis.group11501.smartmuseum.repository.UserRepository;
-        import ru.kpfu.itis.group11501.smartmuseum.util.AuthForm;
-        import sun.util.resources.cldr.guz.LocaleNames_guz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.kpfu.itis.group11501.smartmuseum.model.User;
+import ru.kpfu.itis.group11501.smartmuseum.repository.UserRepository;
+import ru.kpfu.itis.group11501.smartmuseum.util.AuthForm;
 
 /**
  * Created by Bogdan Popov on 05.11.2017.
@@ -34,7 +33,7 @@ public class SignInController {
     @RequestMapping(value = "/create_user")
     public String createUser() {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
-        User u = new User(encoder.encode("1234"),"kolya","kolya","volkov","","",true, 1L,2L,null);
+        User u = new User(encoder.encode("1234"), "kolya", "kolya", "volkov", "", "", true, 1L, 2L, null);
         userRepository.save(u);
         return "sign_in";
     }
