c06477d53ab316b418c5ce8b77104c606e3f5749
Bogdan <ibogdanpopov@gmail.com>
2018-04-13 09:51:50 +0300

    Role and Position entities were added

---
+++ src/main/java/ru/kpfu/itis/group11501/smartmuseum/model/Role.java
@@ -0,0 +1,34 @@
+package ru.kpfu.itis.group11501.smartmuseum.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Bogdan Popov on 13.04.2018.
+ */
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
