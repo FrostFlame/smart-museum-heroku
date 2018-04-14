b6d808906c4fd76e6e76c6cc48a1debd2d6f8266
Volkov Kolya <007vampir007@gmail.com>
2018-04-15 01:01:00 +0300

    fix hql

--- src/main/java/ru/kpfu/itis/group11501/smartmuseum/repository/ExpositionRepository.java
+++ src/main/java/ru/kpfu/itis/group11501/smartmuseum/repository/ExpositionRepository.java
@@ -8,6 +8,6 @@ import ru.kpfu.itis.group11501.smartmuseum.model.Exposition;
  * Created by volkov on 13.04.2018.
  */
 public interface ExpositionRepository extends JpaRepository<Exposition, Long> {
-    @Query("select e from Exposition as e where e.id = (select min(e.id) from Exposition ) ")
+    @Query("select e from Exposition as e where e.id = (select min(e2.id) from Exposition e2 ) ")
     Exposition getFirstExposition();
 }
